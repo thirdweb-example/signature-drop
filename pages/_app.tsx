bf6a343da436592e174cb95b125b421b31198e3b
Jarrod Watts <jarrodwatts16@gmail.com>
2022-11-08 13:20:29 +1100

    Add viewport meta tag in head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import ThirdwebGuideFooter from "../components/GitHubLink";
+import Head from "next/head";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Goerli;
@@ -9,6 +10,9 @@ const activeChainId = ChainId.Goerli;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
       <ThirdwebGuideFooter />
     </ThirdwebProvider>
