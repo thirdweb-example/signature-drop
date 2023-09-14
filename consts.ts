63981cef3ca91a850e808691c0ce2b90a987ac62
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-09-14 23:07:48 +0530

    feat: update template (#4)
    * feat: update template
    
    * chore: remove lock

---
+++ consts.ts
@@ -0,0 +1,6 @@
+// Replace this with your contract address
+export const EDITION_DROP_ADDRESS =
+  "0xE7407244b890F061EA97fA999fcDab67E4992851";
+
+export const SIGNATURE_DROP_ADDRESS =
+  "0xb5b081A0FD7d6356D3e0AC0d434E82e48CE8F402";
